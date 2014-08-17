764200a67a6233b40b57ffd222435a58ae9127c0
Eddie Ramirez <djedi.r@gmail.com>
2014-08-16 22:55:56 -0500

    Going OOP

--- monitor.py
+++ monitor.py
@@ -5,30 +5,65 @@ import threading
 import mysql.connector
 import csv
 import time
+import argparse
 
-options = dict()
-cxn = False
+
+class GetOptions:
+
+  option_file = None
+  params = dict()  
+
+  def __init__(self, defaults_file = None):
+    if defaults_file is None:
+      self.option_file = "/etc/my.cnf"          
+    else:
+      self.option_file = defaults_file
+
+  def read_options(self):
+    config = SafeConfigParser(allow_no_value = True)
+    config.read(self.option_file)
+    self.params = dict(config.items('yatmym'))
+
+class MySQLMonitor:
+  con = False
+  cursor = False
+  global_status = [] 
+  def __init__(self, u, p, h, P):
+    try:
+      self.con =  mysql.connector.connect(user=u, password=p, host=h, port=P)
+      self.cursor = self.con.cursor()
+    except mysql.connector.Error as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print "Something is wrong with your user name or password"
+      elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print "Database does not exists"
+      else:
+        print err
+    else:
+      self.con.close()
+    
+  def get_global_status(self):
+    query = "SHOW GLOBAL STATUS"
+    self.cursor.execute(query) 
+    self.global_status = self.cursor.fetchall()
+ 
+parser = argparse.ArgumentParser(description="This script captures the GLOBAL STATUS & VARIABLES of a MySQl Server.")
+parser.add_argument("--defaults-file", help="The options file where YaTMyM would find options")
+args = parser.parse_args()
+
+x = GetOptions(defaults_file = args.defaults_file)
+x.read_options()
+options = x.params
+print options
+monitor = MySQLMonitor(u = options["user"], p = options["password"], h = options["host"], P = options["port"])
+monitor.get_global_status()
+
+cxn = monitor.con
+
 file_status = None
 file_variables = None
 
-def read_options():
-  config = SafeConfigParser(allow_no_value = True)
-  config.read('/mysql/5.6.14_3306/my.cnf')
-  print config.items('yatmom')
 
-
-def connect_mysql():
-  try:
-    return mysql.connector.connect(user='yatmom', password='dummypass', host='127.0.0.1')
-  except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-      print "Something is wrong with your user name or password"
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-      print "Database does not exists"
-    else:
-      print err
-  else:
-    cnx.close()
 
 def _append_to_file(line, f):
   myfile = open(f, 'a')
@@ -67,18 +102,11 @@ def _rows_to_list(rows):
     values.append(row[1])              
   return values
 
-def _get_global_status():
-  query = "SHOW GLOBAL STATUS"
-  cursor = cnx.cursor()
-  cursor.execute(query) 
-  return cursor.fetchall()
 
 def monitor_server(interval=1):
   v =  _rows_to_list(_get_global_status())
   _append_to_file(v,"status.csv")
   thread = threading.Timer(1, monitor_server).start()
 
-read_options()
-cnx = connect_mysql()
 create_log_file("status.csv", _get_global_status())
 monitor_server(1)
