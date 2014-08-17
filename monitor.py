c39ed92f818968944c5c47e0669f58724541c158
Eddie Ramirez <djedi.r@gmail.com>
2014-08-16 23:50:16 -0500

    Moved most functions to MYSQLMONITOR CLASS

--- monitor.py
+++ monitor.py
@@ -28,10 +28,13 @@ class MySQLMonitor:
   con = False
   cursor = False
   global_status = [] 
+  global_variables = []
+  interval = 1
   def __init__(self, u, p, h, P):
     try:
       self.con =  mysql.connector.connect(user=u, password=p, host=h, port=P)
       self.cursor = self.con.cursor()
+      self.create_log_file(file_name = "status.csv")
     except mysql.connector.Error as err:
       if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print "Something is wrong with your user name or password"
@@ -39,14 +42,47 @@ class MySQLMonitor:
         print "Database does not exists"
       else:
         print err
-    else:
-      self.con.close()
-    
+  
+  def set_inverval(n):
+    self.interval = n
+
+  def create_log_file(self, file_name):
+    if not os.path.exists(file_name):
+      try:
+        my_file = open(file_name,'a')
+        wr = csv.writer(my_file)
+        wr.writerow(self.rows_to_headers(self.get_global_status()))
+        my_file.close()
+      except IOError:
+        print "Error: File could not be created."
+        return 0
+     
+
+  def rows_to_headers(self,rows):
+    i = 0
+    headers = []
+    for row in rows:
+      headers.append(row[0])
+    headers.insert(0,"Datetime")
+    return headers
+
+  def get_global_variables(self):
+    query = "SHOW GLOBAL VARIABLES"
+    self.cursor.execute(query) 
+    self.global_status = self.cursor.fetchall()
+  
   def get_global_status(self):
     query = "SHOW GLOBAL STATUS"
     self.cursor.execute(query) 
-    self.global_status = self.cursor.fetchall()
+    return self.cursor.fetchall()
  
+def _rows_to_list(rows):
+  i = 0
+  values = [] 
+  values.append(time.strftime("%Y-%m-%d %H:%H:%S", time.gmtime()))
+  for row in rows:
+    values.append(row[1])              
+  return values
 parser = argparse.ArgumentParser(description="This script captures the GLOBAL STATUS & VARIABLES of a MySQl Server.")
 parser.add_argument("--defaults-file", help="The options file where YaTMyM would find options")
 args = parser.parse_args()
@@ -56,8 +92,9 @@ x.read_options()
 options = x.params
 print options
 monitor = MySQLMonitor(u = options["user"], p = options["password"], h = options["host"], P = options["port"])
+monitor.set_interval = options["interval"]
 monitor.get_global_status()
-
+print  monitor.global_status
 cxn = monitor.con
 
 file_status = None
@@ -65,48 +102,9 @@ file_variables = None
 
 
 
-def _append_to_file(line, f):
-  myfile = open(f, 'a')
-  wr = csv.writer(myfile, quoting=csv.QUOTE_MINIMAL)  
-  wr.writerow(line)
-
-def create_log_file(file_name, headers):
-  if not os.path.exists(file_name):
-    try:
-      my_file = open(file_name, 'a')
-      wr = csv.writer(my_file)
-      wr.writerow(_rows_to_headers(headers))
-      my_file.close()
-    except IOError:
-      print "Error: File could not be created."
-      return 0
-    
-def set_headers(headers, file_name):
-  my_file = open(file_name, 'a')
-  wr = csv.writer(my_file)
-  wr.writerow(_rows_to_headers(_get_global_status()))
- 
-def _rows_to_headers(rows):
-  i = 0
-  headers = []
-  for row in rows:
-    headers.append(row[0])
-  headers.insert(0,"Datetime")
-  return headers
-
-def _rows_to_list(rows):
-  i = 0
-  values = [] 
-  values.append(time.strftime("%Y-%m-%d %H:%H:%S", time.gmtime()))
-  for row in rows:
-    values.append(row[1])              
-  return values
-
 
 def monitor_server(interval=1):
-  v =  _rows_to_list(_get_global_status())
+  v = _rows_to_list(_get_global_status())
   _append_to_file(v,"status.csv")
   thread = threading.Timer(1, monitor_server).start()
 
-create_log_file("status.csv", _get_global_status())
-monitor_server(1)
